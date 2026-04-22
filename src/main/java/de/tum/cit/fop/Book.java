0d6613d15029ce7f5b12d5c973e5a771576b6e5d
Asith Mallika Kankanamalage <93966973+Asithmk@users.noreply.github.com>
2026-04-22 09:14:20 +0200

    Update Book.java

--- src/main/java/de/tum/cit/fop/Book.java
+++ src/main/java/de/tum/cit/fop/Book.java
@@ -1,1 +1,39 @@
+package de.tum.cit.fop;
+
+public class Book {
+    private String title;
+    private String author;
+    private String publisher;
+    private int year;
+    private String genre;
+    private int id;
+
+    public Book(String title, String author, String publisher, int year, String genre, int id) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.year = year;
+        this.genre = genre;
+        this.id = id;
+    }
+
+    // Getters and setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+    public String getPublisher() { return publisher; }
+    public void setPublisher(String publisher) { this.publisher = publisher; }
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    @Override
+    public String toString() {
+        return title + " (ID: " + id + ")";
+    }
+}
 
